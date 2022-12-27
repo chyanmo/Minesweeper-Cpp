1a2a5d70be5a98a10ff167296f42e22b173d4b99
chyanmo <1831312920@qq.com>
2022-12-28 00:53:13 +0800

    refactor(Button): 调整继承关系，BasicWidget->Label->Button

--- minesweeper/Button.cpp
+++ minesweeper/Button.cpp
@@ -1,19 +1,7 @@
 ﻿#include "Button.h"
 
 Button::Button(int x, int y, int w, int h)
-    :BasicWidget(x,y,w,h) {}
-
-void Button::show()
-{
-    setfillcolor(current_clr);
-    ::fillroundrect(m_x, m_y, m_x + m_w, m_y + m_h, 10, 10);
-
-    settextcolor(text_clr);
-    // 文字居中
-    int tx = m_x + (m_w - textwidth(m_text.c_str())) / 2,
-        ty = m_y + (m_h - textheight(m_text.c_str())) / 2;
-    ::outtextxy(tx, ty, m_text.c_str());
-}
+    :Label(x,y,w,h) {}
 
 bool Button::isin()
 {
@@ -37,12 +25,14 @@ unsigned short Button::is_clicked()
     return 0;
 }
 
+
+
 void Button::eventLoop(const ExMessage& msg)
 {
     m_msg = msg;
     if (!isin())
-        current_clr = normal_clr;
-    else current_clr = hover_clr;
+        bk_clr = normal_clr;
+    else bk_clr = hover_clr;
 }
 
 void Button::setBkClr(COLORREF c)
@@ -53,14 +43,4 @@ void Button::setBkClr(COLORREF c)
 void Button::setHoverClr(COLORREF c)
 {
     hover_clr = c;
-}
-
-void Button::setText(const std::string& text)
-{
-    m_text = text;
-}
-
-void Button::setTextClr(COLORREF c)
-{
-    text_clr = c;
 }
--- minesweeper/Button.h
+++ minesweeper/Button.h
@@ -1,33 +1,35 @@
 ﻿#pragma once
 #include "configer.h"
-#include "BasicWidget.h"
+#include "Label.h"
 
 class Button :
-    public BasicWidget
+    public Label
 {
 public:
     Button(int x = 0, int y = 0, int w = 10, int h = 10);
-    void show() override;
 
-    void setText(const std::string & = std::string());  // 设置文本
-    void setTextClr(COLORREF);                          // 设置文本颜色
-    void setBkClr(COLORREF);                            // 设置背景颜色
-    void setHoverClr(COLORREF);                         // 设置被鼠标扫过时的颜色
+    void setHoverClr(COLORREF);         // 设置被鼠标扫过时的颜色
+    void setBkClr(COLORREF);            // 设置没有被鼠标扫过时的颜色
+
+    void eventLoop(const ExMessage&);   // 用于接收消息
+
+    bool isin();                        // 判断鼠标在按钮上
+    unsigned short is_clicked();        // 判断点击，左击返回 1，右击返回 2，没点击返回 0
+
     
-    bool isin();                    // 判断鼠标在按钮上
-    unsigned short is_clicked();    // 判断点击，左击返回 1，右击返回 2，没点击返回 0
-
-    void eventLoop(const ExMessage&);
+    /*
+     * 在父类中实现：
+     * 
+     * void show();                                         // 显示
+     * void setText(const std::string & = std::string());   // 设置文本
+     * void setTextClr(COLORREF);                           // 设置文本颜色
+     */
 
 
 private:
     std::string m_text = std::string();
     ExMessage m_msg;
-private:
-    COLORREF text_clr = BLACK;
-    // COLORREF current_clr = RGB(232, 232, 236);
-    COLORREF current_clr = RGB(100, 100, 200);
-    COLORREF normal_clr = RGB(100, 100, 200);
-    COLORREF hover_clr = RED;
-};
 
+    COLORREF normal_clr = RGB(217, 217, 243);
+    COLORREF hover_clr = RGB(206, 239, 228);
+};
