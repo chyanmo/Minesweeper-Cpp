a53eaae9d9a6781d00c71b6aa72c52d6c691531e
chyanmo <1831312920@qq.com>
2022-12-28 00:48:02 +0800

    new(Label): 新增 Label 类，Label 是用于显示文本的组件

---
+++ minesweeper/Label.cpp
@@ -0,0 +1,37 @@
+﻿#include "Label.h"
+
+// Label::Label(int x, int y, int w, int h)
+//    :BasicWidget(x, y, w, h) {}
+    
+Label::Label(int x, int y, int w, int h, const std::string& text, COLORREF textcolor, COLORREF backgroundcolor)
+    :BasicWidget(x, y, w, h), m_text(text), text_clr(textcolor), bk_clr(backgroundcolor) {}
+
+Label::Label(int x, int y, int w, int h, COLORREF backgroundcolor, const std::string& text, COLORREF textcolor)
+    :BasicWidget(x, y, w, h), m_text(text), text_clr(textcolor), bk_clr(backgroundcolor) {}
+
+void Label::show()
+{
+    setfillcolor(bk_clr);
+    ::fillroundrect(m_x, m_y, m_x + m_w, m_y + m_h, 10, 10);
+
+    settextcolor(text_clr);
+    // 文字居中
+    int tx = m_x + (m_w - textwidth(m_text.c_str())) / 2,
+        ty = m_y + (m_h - textheight(m_text.c_str())) / 2;
+    ::outtextxy(tx, ty, m_text.c_str());
+}
+
+void Label::setText(const std::string& text)
+{
+    m_text = text;
+}
+
+void Label::setTextClr(COLORREF c)
+{
+    text_clr = c;
+}
+
+void Label::setBkClr(COLORREF c)
+{
+    bk_clr = c;
+}
---
+++ minesweeper/Label.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "configer.h"
+#include "BasicWidget.h"
+
+class Label :
+    public BasicWidget
+{
+public:
+    Label(int x, int y, int w, int h,
+        const std::string& text = std::string(),
+        COLORREF textcolor = BLACK, COLORREF backgroundcolor = RGB(100, 100, 200));
+
+    Label(int x, int y, int w, int h, COLORREF backgroundcolor,
+        const std::string& text = std::string(),
+        COLORREF textcolor = BLACK);
+
+    void show() override;
+
+    void setText(const std::string & = std::string());  // 设置文本
+    void setTextClr(COLORREF);                          // 设置文本颜色
+    void setBkClr(COLORREF);                            // 设置背景颜色
+
+protected:
+    std::string m_text;
+
+    COLORREF text_clr;
+    COLORREF bk_clr;
+};
